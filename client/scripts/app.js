1fca4e86f656b0754c4796866974babbe5ccd854
Hack Reactor Students <accounts@hackreactor.com>
2017-01-03 16:27:36 -0800

    Add styling, room change functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -37,6 +37,20 @@ $(document).ready(function() {
       context.fetch();
     });
 
+    $('#create-room').on('click', function() {
+      $(this).hide();
+      $('#create-room-text').show();
+    });
+
+    $('#room-submit').on('click', function() {
+      var newRoom = $('#create-room-text').val();
+      context.renderRoom({roomname: newRoom});
+      $('#create-room-text').val('').hide();
+      $('#create-room').show();
+      $('#roomSelect option[value="' + newRoom + '"]').attr('selected', 'selected');
+      context.fetch();
+    });
+
   };
 
   ChatterBox.prototype.send = function (message) {
@@ -59,11 +73,12 @@ $(document).ready(function() {
 
   ChatterBox.prototype.fetch = function () {
     var room = $('#roomSelect').find(':selected').text();
+    var dataValue = room === 'All' ? {'order': '-createdAt'} : {'order': '-createdAt', 'where': '{"roomname":"' + room + '"}'};
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: this.server,
       type: 'GET',
-      data: {'order': '-createdAt'},
+      data: dataValue,
       success: function (data) {
         this.clearMessages();
         for (var i = data.results.length - 1; i >= 0; i--) {
@@ -84,7 +99,7 @@ $(document).ready(function() {
   };
 
   ChatterBox.prototype.clearMessages = function () {
-    $('.messages > div').remove();
+    $('.messages > li').remove();
   };
 
   ChatterBox.prototype.renderMessage = function (message) {
@@ -92,21 +107,21 @@ $(document).ready(function() {
     var month = created.getMonth() + 1;
     var day = created.getDate();
     var year = created.getFullYear();
-    var hours = created.getHours();
-    var mins = created.getMinutes();
-    var seconds = created.getSeconds();
-    var milli = created.getMilliseconds();
+    var hours = ('0' + created.getHours()).slice(-2);
+    var mins = ('0' + created.getMinutes()).slice(-2);
+    var seconds = ('0' + created.getSeconds()).slice(-2);
 
     var username = this.escapeHtml(message.username);
-    var timeStamp = month + '/' + day + '/' + year + ' ' + hours + ':' + mins + ':' + seconds + ':' + milli;
+    var timeStamp = month + '/' + day + '/' + year + ' ' + hours + ':' + mins + ':' + seconds;
     var user = $('<span></span>').append(username + ' foretold: ');
     var text = $('<span></span>').append(this.escapeHtml(message.text));
     var time = $('<span></span>').append(timeStamp);
     var listItem = $('<li></li>');
 
-    user.addClass(username.replace(/ /g, '__'));
+    user.addClass(username.replace(/ /g, '__').replace('%20', ''));
     if (this.friends.indexOf(username.replace(/ /g, '__')) >= 0) {
       user.addClass('friend');
+      listItem.addClass('friend-box');
     }
     listItem.append(user).append(text).append(time);
     $('.messages').prepend(listItem);
@@ -117,7 +132,7 @@ $(document).ready(function() {
     var rooms = $('#roomSelect').children('option');
 
     for (var i = 0; i < rooms.length; i++) {
-      if (rooms[i].value === currentRoom) {
+      if (rooms[i].value === currentRoom || currentRoom.includes('script') || currentRoom.includes('&quot')) {
         return;
       }
     }
@@ -125,7 +140,7 @@ $(document).ready(function() {
   };
 
   ChatterBox.prototype.escapeHtml = function (string) {
-    // return string;
+    //return string;
     var entityMap = {
       '&': '&amp;',
       '<': '&lt;',
@@ -143,7 +158,7 @@ $(document).ready(function() {
   ChatterBox.prototype.handleUsernameClick = function (username) {
     var index = this.friends.indexOf(username);
     $('.' + username).toggleClass('friend');
-    console.log($('.' + username).parent().toggleClass('friend-box'));
+    $('.' + username).parent().toggleClass('friend-box');
 
     if (index >= 0) {
       this.friends.splice(index, 1);
@@ -154,6 +169,6 @@ $(document).ready(function() {
 
   app = new ChatterBox();
   app.init();
-  // setInterval(app.fetch.bind(app), 2000);
+  setInterval(app.fetch.bind(app), 2000);
 
 });
